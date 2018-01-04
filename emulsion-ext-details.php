d89b9f368c19b553f03da967f10c432f8b0992c6
GideonAgboba <gideonju1738@gmail.com>
2018-01-04 11:37:38 -0800

    version 1.0

---
+++ emulsion-ext-details.php
@@ -0,0 +1,57 @@
+<div class="modal fade details-4" id="details-4" tabindex="-1" role="dialog" aria-labelledby="details-4" aria-hidden="true">
+
+	<div class="modal-dialog model-lg" home-modal>
+		<div class="modal-content home-modal">
+			<div class="modal-header">
+				<button class="close" type="button" data-dismiss="modal" aria-label="close">
+				<span aria-hidden="true">&times;</span>
+				</button>
+				<h4 class="modal-title text-center">White and colored emulsion</h4>
+			</div>
+
+			<div class="modal-body">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-lg-6">
+							<div class="center-block">
+								<img src="imgs/emulsion-int.jpg" alt="interior emulsions" class="details img-responsive" />
+							</div>
+						</div>
+
+						<div class="col-lg-6">
+							<h4>Details</h4>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore.</p>
+							<hr>
+							<p>Price: $8,500</p>
+							<p>Brand: emulsion</p>
+
+							<form accept="add-to-cart.php" method="post">
+								<div class="form-group mb-0">
+									<div class="col-xs-3" id="quantity-label">
+										<label for="quantity">Quantity: </label>
+										<input type="number" name="quantity" id="quantity" class="form-control" />
+									</div>
+									<div class="mt-0">
+										<label for="type">Type: </label>
+										<select class="text-success text-center form-control" id="type" name="type">	
+											<option value=""></option>
+											<option value="">Off-white</option>
+											<option value="">Cyan</option>
+											<option value="">Aquammarin</option>
+										</select>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+				<div class="modal-footer">
+					<button class="btn btn-default" data-dismiss="modal">Close</button>	
+					<button type="submit" class="btn btn-success"><span class="fa fa-shopping-cart"></span> Add to cart</button>
+				</div>
+		</div>
+	</div>
+
+</div>
---
+++ index.php
@@ -0,0 +1,175 @@
+<! DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+    <title>Sparkle | Home</title>
+    <link href="sparkle-min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <!-- our navbar -->
+     <nav class="navbar navbar-expand-sm home-navbar fixed-top">
+      <a class="navbar-brand home-navbar-logo" href="#"><img src="imgs/nav-logo.png"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample03">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link home-navbar-link" href="#"><i class="fa fa-shopping-cart my-auto"></i> Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link home-navbar-link" href="#"><i class="fa fa-flask"></i> Lab</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link home-navbar-link" href="#"><i class="fa fa-archive"></i> Store</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link home-navbar-link dropdown-toggle" href="http://example.com" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-book"></i> Catalogue</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown03">
+              <a class="dropdown-item text-success text-center" href="#">Emulsion</a>
+              <a class="dropdown-item text-success text-center" href="#">Texcot</a>
+              <a class="dropdown-item text-success text-center" href="#">Gloss</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="home-top-section col-lg-12" >
+      <div class="col-md-2">      </div>
+
+
+
+      <!-- ############## -->
+        <div class="row home-item-sec">
+          <h4 class="col-lg-12 text-center text-success">INTERIOR PAINTS<hr></h4>
+          <div class="col-lg-4">
+            <img src="imgs/emulsion-int.jpg" alt="jeans" id="images" />
+            <p class="text-success">White and colored emulsion</p>
+            <i class="list-price text-danger">Price <s>$5,500</s></i>
+            <p class="price text-success">our price: $4,200</p>
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-1">Details</button>
+          </div>
+
+
+          <div class="col-lg-4">
+            <img src="imgs/texcote-int.jpg" alt="jeans" id="images" />
+            <p class="text-success">White and colored texcote</p>
+            <i class="list-price text-danger">Price <s>$7,000</s></i>
+            <p class="price text-success">our price: $6,000</p>
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-2">Details</button>
+          </div>
+
+
+          <div class="col-lg-4">
+            <img src="imgs/gloss1.jpg" alt="jeans" id="images" />
+            <p class="text-success">White and colored gloss</p>
+            <i class="list-price text-danger">Price <s>$4,990</s></i>
+            <p class="price text-success">our price: $4,500</p>
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-3">Details</button>
+          </div>
+
+        </div>
+
+
+
+        <div class="row home-item-sec">
+          <h4 class="col-lg-12 text-center text-success">EXTERIOR PAINTS<hr></h4>
+          <div class="col-lg-4 mb-4">
+            <img src="imgs/emulsion-int.jpg" alt="jeans" id="images" />
+
+            <p class="text-success">White and colored emulsion</p>
+            <i class="list-price text-danger">Price <s>$10,000</s></i>
+            <p class="price text-success">Our price: $8,500</p>
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-4">Details</button>
+          </div>
+
+
+          <div class="col-lg-4">
+            <img src="imgs/texcote2.jpg" alt="jeans" id="images" />
+            <p class="text-success">White and colored texcote</p>
+            <i class="list-price text-danger">Price <s>$12,000</s></i>
+            <p class="price text-success">Our price: $10,500</p>
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-5">Details</button>
+          </div>
+
+
+          <div class="col-lg-4">
+            <img src="imgs/gloss2.jpg" alt="jeans" id="images" />
+            <p class="text-success">White and colored gloss</p>
+            <i class="list-price text-danger">Price <s>$4.990</s></i>
+            <p class="price text-success">Our price: $4,500  </p>
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-6">Details</button>
+          </div>
+
+        </div>
+
+      </div>
+
+      <div class="container home-contact-section text-center">
+        <a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</a>
+        <br>
+        <button type="button" class="btn btn-success btn-lg">Contact us</button>
+      </div>
+
+
+      <div class="home-about-section">
+        <div class="home-about-section-container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="rounded-circle home-about-img">
+                        <img src="imgs/ss.jpg" class="rounded-circle">
+                    </div>
+                </div>
+                <div class="col-lg-8 text-center text-success">
+                    <q>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, laborum.
+                    </q><br><br>
+                        <a href="#" class="btn btn-success btn-lg">Contact Us</a>
+                </div>
+            </div>
+        </div>
+      </div>
+
+      <div class="container home-contact-section text-center">
+
+      </div>
+
+
+      <footer class="home-footer bg-success">
+        <div class="text-white text-center">
+          Sparklepaints.co@2018
+          <p>Like our Social platforms@ <a href="#">Sparkle_paints</a></p>
+          <div class="container footer-social">
+            <i class="fa fa-facebook footer-social-i rounded-circle"></i>
+            <i class="fa fa-instagram footer-social-i rounded-circle"></i>
+            <i class="fa fa-twitter footer-social-i rounded-circle"></i>
+          </div>
+        </div>
+      </footer>
+
+      <?php
+        include 'emulsion-int-details.php';
+        include 'texcote-int-details.php';
+        include 'gloss-int-details.php';
+        include 'emulsion-ext-details.php';
+        include 'texcote-ext-details.php';
+        include 'gloss-ext-details.php';
+      ?>
+
+
+
+    <script src="jquery/jquery.min.js"></script>
+    <script src="popper/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
